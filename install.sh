cb62db59082a1fec0189b7ba3f21f8b3f8581715
Sergej Laufer <10756722+sergej-laufer@users.noreply.github.com>
2023-05-23 11:21:45 +0200

    update git credentials

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Optional: Set Git credentials for privat repositories 
+REPO_USER=$GITHUB_USER 
+REPO_PASSWORD=$GITHUB_PAT
+REPO_URL=$GITHUB_URL
+
 if [[ $# -eq 0 ]] ; then
     echo 'Please provide an environment parameter (e.g. dev, stage, prod)'
     exit 1
@@ -24,6 +29,16 @@ helm upgrade --install argocd . \
 echo "Waiting for Deployment to be ready..."
 kubectl wait --for=condition=available --timeout=300s deployment/argocd-server -n argocd
 
+# To use privat Git Repositories, add a Secret with the git credentials 
+# and the `argocd.argoproj.io/secret-type` label.
+if [[ ! -z "$REPO_USER" && ! -z "$REPO_PASSWORD" && ! -z "$REPO_URL" ]]; then
+    kubectl create secret generic git-repo-creds -n argocd \
+    --from-literal=password=$REPO_PASSWORD \
+    --from-literal=url=$REPO_URL \
+    --from-literal=username=$REPO_USER
+
+    kubectl label secret git-repo-creds -n argocd  "argocd.argoproj.io/secret-type=repository"
+fi
 
 # Install BigBang application using the Helm chart from the local repository
 cd ../../..
